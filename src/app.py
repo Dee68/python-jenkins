a26d0fb81aa92725b125d03968a60fbf74f4e71d
Dee68 <ddimie283@gmail.com>
2024-03-29 17:22:38 +0000

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,7 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+
+def say_something():
+    return '<h1>What is wrong with the leaders of today?</h1>'
---
+++ tests/test_app.py
@@ -0,0 +1,4 @@
+from app import say_something
+
+def test_say_something():
+    assert say_something() == "<h1>What is wrong with the leaders of today?</h1>"
